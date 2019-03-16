a87a5f1183959dfaf6ad81260011a2e53dcce68f
rishikesh18 <rishi.neupane@gmail.com>
2019-03-15 22:48:06 -0400

    seeing icon set and able to get next activity

--- app/src/main/java/com/example/umbrella/MainActivity.java
+++ app/src/main/java/com/example/umbrella/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.umbrella;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView settingImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        settingImage =(ImageView) findViewById(R.id.setting_btn);
+
+        settingImage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Settings.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
