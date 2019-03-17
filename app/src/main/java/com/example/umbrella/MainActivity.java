9f07275f5417cd9c020d90c002e184c9355c4874
rishikesh18 <rishi.neupane@gmail.com>
2019-03-16 21:41:52 -0400

    basic layouts and able to get zip code and elected units

--- app/src/main/java/com/example/umbrella/MainActivity.java
+++ app/src/main/java/com/example/umbrella/MainActivity.java
@@ -1,29 +1,48 @@
 package com.example.umbrella;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView settingImage;
+    int userZipCode = 0;
+    String userUnit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         settingImage =(ImageView) findViewById(R.id.setting_btn);
-
         settingImage.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
                 Intent intent = new Intent(MainActivity.this, Settings.class);
-                startActivity(intent);
-
+                startActivityForResult(intent, 1);
             }
         });
+        SharedPreferences sharedPreferences = getSharedPreferences("Mydata", Context.MODE_PRIVATE);
+        userZipCode = sharedPreferences.getInt("zip", 0);
+        userUnit = sharedPreferences.getString("unit", "");
+        if(userZipCode == 0){
+            Intent intent = new Intent(MainActivity.this, Settings.class);
+            startActivityForResult(intent, 1);
+
+        }else{
+           initilizeRetrofit();
+        }
+
+    }
+
+    private void initilizeRetrofit() {
+
+
+
+
     }
 }
--- app/src/main/java/com/example/umbrella/Settings.java
+++ app/src/main/java/com/example/umbrella/Settings.java
@@ -1,13 +1,94 @@
 package com.example.umbrella;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Settings extends AppCompatActivity {
+public class Settings extends Activity implements AdapterView.OnItemSelectedListener {
+    ImageView backBtn;
+    EditText getZip;
+    TextView alertArea;
+    Spinner spinner;
+    String myUnits;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        spinner = (Spinner) findViewById(R.id.choose_units);
+        backBtn =(ImageView) findViewById(R.id.back_btn);
+        getZip = (EditText) findViewById(R.id.enter_zip);
+        alertArea = (TextView) findViewById(R.id.alert);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.temperature_option, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+
+
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try{
+                    String zip_string = getZip.getText().toString();
+                    int user_zip = new Integer(zip_string).intValue();
+
+                    SharedPreferences sharedPreferences= getSharedPreferences("Mydata", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor=sharedPreferences.edit();
+                    editor.putInt("zip", user_zip);
+                    editor.putString("unit", myUnits);
+                    editor.commit();
+
+                }catch (Exception e){
+                    Toast.makeText(Settings.this, "Something went wrong", Toast.LENGTH_LONG).show();
+                }
+
+
+                if(getZip.length() == 5){
+                    Intent intent = new Intent(Settings.this, MainActivity.class);
+                    startActivity(intent);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }else{
+                    alertArea.setText("Enter 5 digit zip code");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                myUnits = "Celsius";
+                break;
+            case 1:
+                myUnits = "Fahrenheit";
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
     }
 }
